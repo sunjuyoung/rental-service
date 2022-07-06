cfdee4aafb2c44b03cda2f6672b03911d16ca476
syseoz <syseoz@naver.com>
2022-07-06 23:21:06 +0900

    test customfilter

---
+++ api-gateway/src/main/java/com/example/apigateway/filter/PostCustomFilter.java
@@ -0,0 +1,33 @@
+package com.example.apigateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class PostCustomFilter extends AbstractGatewayFilterFactory<PostCustomFilter.Config> {
+
+    public PostCustomFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // grab configuration from Config object
+        return (exchange, chain) -> {
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                ServerHttpResponse response = exchange.getResponse();
+                //Manipulate the response in some way
+                log.info("custom post filter : response ={}",response.getStatusCode());
+            }));
+        };
+    }
+
+    public static class Config {
+        //Put the configuration properties for your filter here
+    }
+}
---
+++ api-gateway/src/main/java/com/example/apigateway/filter/PreCustomFilter.java
@@ -0,0 +1,33 @@
+package com.example.apigateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class PreCustomFilter extends AbstractGatewayFilterFactory<PreCustomFilter.Config> {
+
+    public PreCustomFilter() {
+        super(Config.class);
+    }
+
+    public static class Config{
+
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        //custom pre filter
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            log.info("CUstom pre filter : request id : {}", request.getId());
+
+            return chain.filter(exchange.mutate().request(request).build());
+        });
+    }
+}
